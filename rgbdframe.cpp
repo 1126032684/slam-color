b4fdd99d39ecd21109e87024bf921518357e906b
1126032684 <37234646+1126032684@users.noreply.github.com>
2019-03-21 19:33:53 +0800

    Add files via upload

---
+++ rgbdframe.cpp
@@ -0,0 +1,87 @@
+#include "rgbdframe.h"
+#include "common_headers.h"
+#include "parameter_reader.h"
+
+using namespace rgbd_tutor;
+
+RGBDFrame::Ptr   FrameReader::next()
+{
+    switch (dataset_type) {
+    case NYUD:
+        //TODO 增加nyud的接口
+        break;
+    case TUM:
+    {
+        if (currentIndex < start_index || currentIndex >= rgbFiles.size())
+            return nullptr;
+
+        RGBDFrame::Ptr   frame (new RGBDFrame);
+        frame->id = currentIndex;
+        frame->rgb = cv::imread( dataset_dir + rgbFiles[currentIndex]);
+        frame->depth = cv::imread( dataset_dir + depthFiles[currentIndex], -1);
+        frame->timeStamp=Timestamp[currentIndex];
+        if (frame->rgb.data == nullptr || frame->depth.data==nullptr)
+        {
+            // 数据不存在
+            return nullptr;
+        }
+
+        frame->camera = this->camera;
+        currentIndex ++;
+        return frame;
+    }
+    default:
+        break;
+    }
+
+    return nullptr;
+}
+
+void FrameReader::init_tum( ParameterReader& para )
+{
+    dataset_dir = parameterReader.getData<string>("data_source");
+    string  associate_file  =   dataset_dir+"/associate.txt";
+    ifstream    fin(associate_file.c_str());
+    if (!fin)
+    {
+        cerr<<"找不着assciate.txt啊！在tum数据集中这尼玛是必须的啊!"<<endl;
+        cerr<<"请用python assicate.py rgb.txt depth.txt > associate.txt生成一个associate文件，再来跑这个程序！"<<endl;
+        return;
+    }
+
+    while( !fin.eof() )
+    {
+        string s;
+        getline(fin,s);
+	  if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double t;
+            string sRGB, sD;
+            ss >> t;
+            Timestamp.push_back(t);
+            ss >> sRGB;
+            rgbFiles.push_back(sRGB);
+            ss >> t;
+            ss >> sD;
+           depthFiles.push_back(sD);
+        }
+     /*   double rgbTime,depthTime;
+        string  rgbFile,  depthFile;
+        fin>>rgbTime>>rgbFile>>depthTime>>depthFile;
+        if ( !fin.good() )
+        {
+            break;
+        }
+        Timestamp.push_back(rgbTime);
+        rgbFiles.push_back( rgbFile );
+        depthFiles.push_back( depthFile );
+        */
+    }
+
+    cout<<"一共找着了"<<rgbFiles.size()<<"个数据记录哦！"<<endl;
+    camera = parameterReader.getCamera();
+    start_index = parameterReader.getData<int>("start_index");
+    currentIndex = start_index;
+}
